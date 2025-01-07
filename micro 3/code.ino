9c589f2cd8d10017aa19b0dc4586f07f673fc91a
Raheleh kokabi <raheleh123k1384@gmail.com>
2025-01-07 13:01:19 +0330

    Create code.ino

---
+++ micro 3/code.ino
@@ -0,0 +1,23 @@
+#include <Adafruit_Sensor.h>
+#include "DHT.h"
+#define DHTPIN 8   
+#define DHTTYPE DHT11   
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.println(F("°C "));
+
+  delay(1000);
+}
